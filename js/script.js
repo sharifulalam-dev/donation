d5f1f8e3c11c00ec46c102eca9bf6a0a126351db
sharifulalam-dev <sharifulalam.tex@gmail.com>
2024-09-25 04:53:39 +0600

    update reverese

--- js/script.js
+++ js/script.js
@@ -26,9 +26,10 @@ const quotaSubmit = getItem(".quota-submit");
 const quotaInput = getItem(".quota-input");
 
 const totalBudget = getItem(".total-budget");
+const cards = document.querySelectorAll(".card-holder");
 
 blogBtn.addEventListener("click", function () {
-  window.location.href = "./blog.html";
+  window.location = "./blog.html";
 });
 
 donation.addEventListener("click", function (e) {
@@ -45,44 +46,82 @@ history.addEventListener("click", function (e) {
   e.target.classList.add("colorcode");
 });
 
-function helpDonation(placeInput, placeDonate, place) {
-  const donationAmount = +placeInput.value;
-  const currentBudget = textToNumber(totalBudget);
-
-  if (donationAmount > currentBudget) {
-    alert("Insufficient Balance. Please recharge Your Balance");
-  } else if (typeof donationAmount === "number" && donationAmount > 0) {
-    const updatedBudget = currentBudget - donationAmount;
-    const donatedAmount = textToNumber(placeDonate);
-    totalBudget.innerText = updatedBudget;
-    placeDonate.innerText = donatedAmount + donationAmount;
+noakhaliSubmit.addEventListener("click", function (e) {
+  if (+noakhaliInput.value > textToNumber(totalBudget)) {
+    alert("Insufficient Balance");
+  } else if (
+    typeof +noakhaliInput.value === "number" &&
+    +noakhaliInput.value > 0
+  ) {
+    const updatePrice = textToNumber(totalBudget) - +noakhaliInput.value;
+    const donatePrice = textToNumber(noakhaliDonate);
+    totalBudget.innerText = updatePrice;
+    noakhaliDonate.innerText = donatePrice + +noakhaliInput.value;
 
     const div = document.createElement("div");
     const html = `<div class="history-card px-4 py-6 border rounded-lg mb-8">
-            <h3 class="text-xl font-bold mb-3">
-              ${donationAmount} Taka is Donated for Donate for Flood at ${place}, Bangladesh
-            </h3>
-            <p>
-              Date : ${new Date().toString()}
-            </p>
-          </div>`;
+        <h3 class="text-xl font-bold mb-3">
+          ${+noakhaliInput.value} Taka is Donated for famine-2024 at Noakhali, Bangladesh
+        </h3>
+        <p>
+          Date : ${new Date().toString()}
+        </p>
+      </div>`;
     div.innerHTML = html;
     historyPortion.appendChild(div);
 
-    showModal();
+    my_modal_1.showModal();
   } else {
     alert("Invalid Donation amount");
   }
-}
-
-noakhaliSubmit.addEventListener("click", function () {
-  helpDonation(noakhaliInput, noakhaliDonate, "Noakhali");
 });
 
-feniSubmit.addEventListener("click", function () {
-  helpDonation(feniInput, feniDonate, "Feni");
+feniSubmit.addEventListener("click", function (e) {
+  if (+feniInput.value > textToNumber(totalBudget)) {
+    alert("Insufficient Balance");
+  } else if (typeof +feniInput.value === "number" && +feniInput.value > 0) {
+    const updatePrice = textToNumber(totalBudget) - +feniInput.value;
+    const donatePrice = textToNumber(feniDonate);
+    totalBudget.innerText = updatePrice;
+    feniDonate.innerText = donatePrice + +feniInput.value;
+    const div = document.createElement("div");
+    const html = `<div class="history-card px-4 py-6 border rounded-lg mb-8">
+      <h3 class="text-xl font-bold mb-3">
+       ${+feniInput.value} Taka is Donated for Donate for Flood at Feni, Bangladesh
+      </h3>
+      <p>
+        Date : ${new Date().toString()}
+      </p>
+    </div>`;
+    div.innerHTML = html;
+    historyPortion.appendChild(div);
+    my_modal_2.showModal();
+  } else {
+    alert("Invalid Donation amount");
+  }
 });
 
-quotaSubmit.addEventListener("click", function () {
-  helpDonation(quotaInput, quotaDonate, "Quota Movement");
+quotaSubmit.addEventListener("click", function (e) {
+  if (+quotaInput.value > textToNumber(totalBudget)) {
+    alert("Insufficient Balance");
+  } else if (typeof +quotaInput.value === "number" && +quotaInput.value > 0) {
+    const updatePrice = textToNumber(totalBudget) - +quotaInput.value;
+    const donatePrice = textToNumber(quotaDonate);
+    totalBudget.innerText = updatePrice;
+    quotaDonate.innerText = donatePrice + +quotaInput.value;
+    const div = document.createElement("div");
+    const html = `<div class="history-card px-4 py-6 border rounded-lg mb-8">
+      <h3 class="text-xl font-bold mb-3">
+       ${+quotaInput.value} Taka is Donated for famine-2024 at Quota Movement, Bangladesh
+      </h3>
+      <p>
+        Date : ${new Date().toString()}
+      </p>
+    </div>`;
+    div.innerHTML = html;
+    historyPortion.appendChild(div);
+    my_modal_3.showModal();
+  } else {
+    alert("Invalid Donation amount");
+  }
 });
